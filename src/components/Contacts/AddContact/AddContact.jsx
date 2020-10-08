093c6dc2b108eb141630e8c4c2c82702783a4fed
Farid Esedov <esedov_fg@pulsarnpp.ru>
2020-10-08 14:12:39 +0300

    fix form handling

--- src/components/Contacts/AddContact/AddContact.jsx
+++ src/components/Contacts/AddContact/AddContact.jsx
@@ -2,54 +2,54 @@ import React, { useState, useEffect } from 'react'
 import { Input, Form, Button } from 'antd'
 import './AddContact.scss'
 
-export default function AddContact(props) {
+export default function AddContact({ currentContact, handleSubmit, type }) {
 
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [address, setAddress] = useState('')
+  const [fields, setFields] = useState([
+    {name: ['name'], value: currentContact ? currentContact.name : ''},
+    {name: ['phone'], value: currentContact ? currentContact.phone : ''},
+    {name: ['address'], value: currentContact ? currentContact.address: ''}
+  ])
 
   useEffect(() => {
-    if(props.currentContact) {
-      setName(props.currentContact.name)
-      setPhone(props.currentContact.phone)
-      setAddress(props.currentContact.address)
+    if(currentContact) {
+      setFields([
+        {name: ['name'], value: currentContact.name},
+        {name: ['phone'], value: currentContact.phone},
+        {name: ['address'], value: currentContact.address}
+      ])
+    } else {
+      setFields([
+        {name: ['name'], value: ''},
+        {name: ['phone'], value: ''},
+        {name: ['address'], value: ''}
+      ])
     }
-    return () => {
-      setName('')
-      setPhone('')
-      setAddress('')
-    }
-  }, [props.currentContact])
+  }, [currentContact])
 
-  const handleNameChange = event => {
-    setName(event.target.value)
-  }
-  const handlePhoneChange = event => {
-    setPhone(event.target.value)
-  }
-  const handleAddressChange = event => {
-    setAddress(event.target.value)
+  const handleFieldsChange = (changed, all) => {
+    setFields(all)
   }
 
   return (
     <Form 
       labelCol={{span: 4}} 
       className="new-contact__form" 
-      name="contact" onFinish={props.handleSubmit}
-      initialValues={props.currentContact ? 
-        {name: props.currentContact.name, phone: props.currentContact.phone, address: props.currentContact.address} : {name: '', phone: '',}}
+      name="contact" 
+      fields={fields}
+      onFinish={handleSubmit}
+      onFieldsChange={handleFieldsChange}
     >
       <Form.Item label="ФИО" name="name" rules={[{ required: true, message: 'Это обязательное поле' }]}>
-        <Input onChange={handleNameChange} value={name} />
+        <Input />
       </Form.Item>
       <Form.Item label="Телефон" name="phone">
-        <Input onChange={handlePhoneChange} value={phone} />
+        <Input />
       </Form.Item>
       <Form.Item label="Адрес" name="address">
-        <Input onChange={handleAddressChange} value={address} />
+        <Input />
       </Form.Item>
       <Form.Item>
-        <Button block type="primary" htmlType="submit">{props.type === 'create' ? "Создать" : "Изменить"}</Button>
+        <Button block type="primary" htmlType="submit">{type === 'create' ? "Создать" : "Изменить"}</Button>
       </Form.Item>
     </Form>
   )
